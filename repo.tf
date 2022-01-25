0d9bb29289995b2dbd8001b70ec90662a8cfbff7
dleblanc-databricks <89589701+dleblanc-databricks@users.noreply.github.com>
2022-01-25 15:42:20 -0500

    Update repo.tf

--- repo.tf
+++ repo.tf
@@ -1,4 +1,3 @@
 resource "databricks_repo" "dewd" {
   url = "https://github.com/databricks-academy/data-engineering-with-databricks.git"
-  path = "/Repos/${data.databricks_current_user.me.home}/DEWD_repo"
 }
