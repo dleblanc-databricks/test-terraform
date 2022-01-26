345df92f0ea9ac7f6b721d9645492c4de6e66c45
dleblanc-databricks <89589701+dleblanc-databricks@users.noreply.github.com>
2022-01-26 15:41:32 -0500

    added pipeline notebook path

--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "notebook_path" {
   default     = "Data-Engineering-With-Databricks/01 - Databricks Lakehouse Platform/DE 1.3.1 - Managing Delta Tables"
 }
 
+variable "dlt_notebook_path" {
+  description = "Pipeline Notebook Relative Path"
+  default     = "Data-Engineering-With-Databricks/03 - Incremental Data and Delta Live Tables/DE 3.3.2 - SQL for Delta Live Tables"
+}
+
 variable "course_name" {
   description = "DBFS/Database Namespace Separator"
   default     = "dewd"
