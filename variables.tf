bcc886d435eb88acb9ea2f5eb5b9c0cb5b58a6dc
dleblanc-databricks <89589701+dleblanc-databricks@users.noreply.github.com>
2022-01-26 09:15:16 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "repo_url" {
+  description = "Databricks Repo URL"
+  default     = "https://github.com/databricks-academy/data-engineering-with-databricks.git"
+}
+
+variable "notebook_path" {
+  description = "Notebook Relative Path"
+  default     = "Data-Engineering-With-Databricks/01 - Databricks Lakehouse Platform/DE 1.3.1 - Managing Delta Tables"
+}
