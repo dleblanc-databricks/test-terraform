2a11522f5f637358c359be5525e1ee3b2981d6c4
dleblanc-databricks <89589701+dleblanc-databricks@users.noreply.github.com>
2022-01-26 15:47:02 -0500

    updated storage

--- pipeline.tf
+++ pipeline.tf
@@ -1,6 +1,6 @@
 resource "databricks_pipeline" "pipeline" {
   name    = "Pipeline Name"
-  storage = "${data.databricks_current_user.me.alphanumeric}/dbacademy/${var.course_name}"
+  storage = "${data.databricks_current_user.me.home}/dbacademy/${var.course_name}"
   target = "dbacademy_${data.databricks_current_user.me.alphanumeric}_${var.course_name}"
 
   library {
