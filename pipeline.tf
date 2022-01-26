227f31f0ca18adfcb630aacccbfad150a3a92e9a
dleblanc-databricks <89589701+dleblanc-databricks@users.noreply.github.com>
2022-01-26 15:43:24 -0500

    Create pipeline.tf

---
+++ pipeline.tf
@@ -0,0 +1,18 @@
+resource "databricks_pipeline" "pipeline" {
+  name    = "Pipeline Name"
+  storage = "${data.databricks_current_user.me.alphanumeric}/dbacademy/${var.course_name}"
+  target = "dbacademy_${data.databricks_current_user.me.alphanumeric}_${var.course_name}"
+
+  library {
+    notebook {
+      path = "${databricks_repo.repo.path}/${var.dlt_notebook_path}"
+    }
+  }
+
+  filters {
+    include = ["com.databricks.include"]
+    exclude = ["com.databricks.exclude"]
+  }
+
+  continuous = false
+}
