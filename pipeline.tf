77932749b775bec3e25dbef72ad32a9e536d576d
dleblanc-databricks <89589701+dleblanc-databricks@users.noreply.github.com>
2022-01-27 14:48:35 -0500

    Update pipeline.tf

--- pipeline.tf
+++ pipeline.tf
@@ -1,3 +1,8 @@
+variable "dlt_notebook_path" {
+  description = "Pipeline Notebook Relative Path"
+  default     = "Data-Engineering-With-Databricks/03 - Incremental Data and Delta Live Tables/DE 3.3.2 - SQL for Delta Live Tables"
+}
+
 resource "databricks_pipeline" "pipeline" {
   name    = "Pipeline Name"
   storage = "${data.databricks_current_user.me.home}/dbacademy/${var.course_name}"
